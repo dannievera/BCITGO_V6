ee4843fefaa3c80ae8b23d9cd7d7ea0d21ae4929
dannievera <buenviajedannielyn@gmail.com>
2025-05-05 05:07:08 -0700

    Working link for register and loin

--- BCITGO_V6/Pages/Account/Login.cshtml.cs
+++ BCITGO_V6/Pages/Account/Login.cshtml.cs
@@ -1,12 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace BCITGO_V6.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        public LoginInputModel Input { get; set; } // Changed type of Input from object to LoginInputModel
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Dummy login validation
+            if (Input.Email == "test@my.bcit.ca" && Input.Password == "password")
+            {
+                TempData["Success"] = "Login success (simulation)";
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return Page();
+            }
+        }
+
+        public class LoginInputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
         }
     }
 }
